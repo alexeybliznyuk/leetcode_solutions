2eb8751e70f35b609caf8df9b00f24e8653a94a1
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2023-01-29 22:49:45 +0300

    Works incorrect

---
+++ romanina_numerals.py
@@ -0,0 +1,54 @@
+#input_int = int(input('Type your num'))
+input_int = 921 #for quick debug
+
+
+
+
+def IndianToRoman(input_int):
+    temp_num = input_int
+    output_roman = ''
+
+    while temp_num > 0:
+#        print("in while")
+        if temp_num >= 1000:
+            output_roman = output_roman + "M"
+            temp_num = temp_num - 1000
+        elif temp_num >= 500:
+            output_roman = output_roman + "D"
+            temp_num = temp_num - 500
+        elif temp_num >= 100:
+            output_roman = output_roman + "C"
+            temp_num = temp_num - 100
+        elif temp_num >= 50:
+            output_roman = output_roman + "L"
+            temp_num = temp_num - 50
+        elif temp_num >= 10:
+            output_roman = output_roman + "X"
+            temp_num = temp_num - 10
+        elif temp_num >= 5:
+            output_roman = output_roman + "V"
+            temp_num = temp_num - 5
+        elif temp_num >= 1:
+            output_roman = output_roman + "I"
+            temp_num = temp_num - 1
+        print(temp_num)
+    return output_roman
+
+
+print("before")
+print(IndianToRoman(input_int))
+print("after")
+            
+        
+
+
+
+'''
+I = 1
+V = 5
+X             10
+L             50
+C             100
+D             500
+M             1000
+'''
