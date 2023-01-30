2ea48869757b2fc070a31203d0289f3ffb2162fd
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2023-01-30 23:50:26 +0300

    Indian to Roman now works correct!

--- romanina_numerals.py
+++ romanina_numerals.py
@@ -1,5 +1,5 @@
-#input_int = int(input('Type your num'))
-input_int = 921 #for quick debug
+input_int = int(input('Type your num'))
+#input_int = 921 #for quick debug
 
 
 
@@ -13,21 +13,39 @@ def IndianToRoman(input_int):
         if temp_num >= 1000:
             output_roman = output_roman + "M"
             temp_num = temp_num - 1000
+        elif temp_num >= 900:
+            output_roman = output_roman + "CM"
+            temp_num = temp_num - 900
         elif temp_num >= 500:
             output_roman = output_roman + "D"
             temp_num = temp_num - 500
+        elif temp_num >= 400:
+            output_roman = output_roman + "CD"
+            temp_num = temp_num - 400
         elif temp_num >= 100:
             output_roman = output_roman + "C"
             temp_num = temp_num - 100
+        elif temp_num >= 90:
+            output_roman = output_roman + "XC"
+            temp_num = temp_num - 90
         elif temp_num >= 50:
             output_roman = output_roman + "L"
             temp_num = temp_num - 50
+        elif temp_num >= 40:
+            output_roman = output_roman + "XL"
+            temp_num = temp_num - 40
         elif temp_num >= 10:
             output_roman = output_roman + "X"
             temp_num = temp_num - 10
+        elif temp_num >= 9:
+            output_roman = output_roman + "IX"
+            temp_num = temp_num - 9
         elif temp_num >= 5:
             output_roman = output_roman + "V"
             temp_num = temp_num - 5
+        elif temp_num >= 4:
+            output_roman = output_roman + "IV"
+            temp_num = temp_num - 4    
         elif temp_num >= 1:
             output_roman = output_roman + "I"
             temp_num = temp_num - 1
