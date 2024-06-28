52f4f2846e94479fedeb20e54778a3acecbcdb59
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2024-06-29 00:57:15 +0300

    Update two_sum_line.cpp

--- two_sum_line.cpp
+++ two_sum_line.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::vector <std::vector <int>>  paired_vector;
-        for (int i = 0; i != nums.size() ; i++) {
-            paired_vector.push_back({nums[i], i});
+        std::vector <int> legacy_nums = nums;
+
+        std::sort(nums.begin(),nums.end() );
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r) {
+            if (nums[l] + nums[r] > target) {
+                r = r - 1;
+            }
+            else if (nums[l] + nums[r] < target) {
+                l++;
+            }
+            else {
+                break;
+            }
         }
-        std::sort(nums.begin(), nums.end());
+        int new_l = -1;
+        int new_r = -1;
 
-	    int l = 0;
-	    int r = nums.size() - 1;
-	    while (l<r) {
-		    if (nums[l] + nums[r] > target) {
-			    r--;
-		    }
-		    else if (nums[l] + nums[r] < target) {
-			    l++;
-		    }
-		    else if (nums[l] + nums[r] == target) {
-			    break;
-		    }
+        for (int i = 0; i != nums.size(); i++) {
+            if (legacy_nums[i] == nums[l] && new_l == -1) {
+                new_l = i;
+            }
+            else if (legacy_nums[i] == nums[r]) {
+                new_r = i;
+            }
 
-	    }
-        int new_r = 0;
-        int new_l = 0;
+        }
+        return {new_l, new_r};
+
+
         
-        for (int k = 0; k != nums.size(); k++) {
-            
-            if (nums[r] == paired_vector[k][0]) {
-                //new_r = k;
-                //new_r = paired_vector[k][1];
-                new_r = k;
-                
-            }
-            else if (nums[l] == paired_vector[k][0]) {
-                //new_l = k;
-                //new_l = paired_vector[k][1];
-                new_l = k;
-            }
-            
-        }
-	    
-	return {new_l, new_r};
 
-}
-    //}
+
+
+
+
+
+    }
 };
