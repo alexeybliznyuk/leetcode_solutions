f6c0935c6cc4fcb7fd3c9f1a63eef8459f48ff87
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2024-07-01 17:16:39 +0200

    Update group_anagrams.cpp

--- group_anagrams.cpp
+++ group_anagrams.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-        //aint working still
+        //still aint working
         std::unordered_map <string, std::vector<std::string>> hmap;
         for (int i = 0; i != strs.size(); i++) {
             std::string temp_strs = strs[i];
-            std::sort(strs[i].begin(), strs[i].end())
+            std::sort(strs[i].begin(), strs[i].end());
             
             int arr[26] = {0};
-            for (int l = 0; l != strs[l].size(), l++) {
+            for (int l = 0; l != strs[l].size(); l++) {
                 //int arr[26] = {0};
                 arr[strs[l] - 97]++;
 
@@ -23,6 +21,14 @@ public:
             hmap[str_for_hm].push_back(temp_strs);
         }
         
+        for (auto ft : hmap) {
+            for (int f = 0; f != hmap[ft].size(); f++) {
+                std::cout << hmap[ft[f]];
+            }
+            std::cout << std::endl;
+        }
         
     }
 };
+
+
