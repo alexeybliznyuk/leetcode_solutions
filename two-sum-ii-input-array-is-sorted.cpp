42cfa41e9f8720dcef6fbf03295659270f24eec1
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2024-07-12 17:07:26 +0200

    Create two-sum-ii-input-array-is-sorted.cpp
    it does work

---
+++ two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        std::string clear_str = "";
+        //for (int i = 0; i != s.size(); i++) {
+        int i = 0;
+        int l = s.size();
+        while (i != s.size()) {
+
+            if (std::isdigit(s[i])) {
+                clear_str+=s[i];
+            }
+            else if (std::isalpha(s[i])) {
+                clear_str += std::tolower(s[i]);
+            
+            }
+            i++;
+        }
+        std::string clear_str2 = "";
+        while (l != -1) {
+
+            if (std::isdigit(s[l])) {
+                clear_str2+=s[l];
+            }
+            else if (std::isalpha(s[l])) {
+                clear_str2 += std::tolower(s[l]);
+            }
+
+
+
+
+            l-=1; 
+            
+        }
+        //std::cout << clear_str << "    " << clear_str2;
+    std::cout << clear_str << "    " << clear_str2;
+    if (clear_str != clear_str2) {
+        return false;
+    }
+    return true;
+    }
+};
