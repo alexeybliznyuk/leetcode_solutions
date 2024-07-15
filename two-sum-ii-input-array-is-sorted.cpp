cb8e95b09753117bc07df17aa06892114b39f012
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2024-07-15 15:17:14 +0200

    Update two-sum-ii-input-array-is-sorted.cpp

--- two-sum-ii-input-array-is-sorted.cpp
+++ two-sum-ii-input-array-is-sorted.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+
+
+        while (l <= r) {
+            if (numbers[l] + numbers[r] > target ) {
+                r-=1;
+            }
+            else if (numbers[l] + numbers[r] < target) {
+                l+=1;
+            }
+            else {
+                return {l+1, r+1};
+            }
+        }
+    return {l, r};
+    }
+};
+
+
+//i put the code from diff problem lmao
+
+/*
+class Solution {
+public:
     bool isPalindrome(string s) {
         std::string clear_str = "";
         //for (int i = 0; i != s.size(); i++) {
@@ -40,3 +66,4 @@ public:
     return true;
     }
 };
+*/
